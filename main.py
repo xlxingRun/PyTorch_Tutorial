82783ce5648168e98c90690887296dd8d9ac23ab
xxl <549506247@qq.com>
2021-08-25 23:17:21 +0800

    add readme.md

--- main.py
+++ main.py
@@ -1,100 +1,45 @@
 # 读取数据集，并进行归一化处理
 import torch
-import torchvision
-import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
-import numpy as np
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
-
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)
-
-testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                         shuffle=False, num_workers=2)
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+from net import Net
+from dataset import train_loader, test_loader
 
 
-# functions to show an image
-def imshow(img):
-    img = img / 2 + 0.5
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
+if __name__ == '__main__':
+    # 定义一个net网络模型
+    net = Net()
+    # 定义损失函数
+    criterion = nn.CrossEntropyLoss()
 
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-# get some random training images
-dataiter = iter(trainloader)
-images, labels = dataiter.next()
+    for epoch in range(2):  # loop over the dataset multiple times
 
-# show images
-imshow(torchvision.utils.make_grid(images))
-# print labels
-print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+        running_loss = 0.0
+        for i, data in enumerate(train_loader, 0):
+            inputs, labels = data
+            optimizer.zero_grad()  # 梯度置零
+            outputs = net(inputs)  # 前向传播
+            loss = criterion(outputs, labels)  # 计算损失
+            loss.backward()        # 后向传播
+            optimizer.step()       # 更新模型参数
 
+            running_loss += loss.item()
+            if i % 2000 == 1999:
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+    print('Finished Training!')
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
 
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-
-net = Net()
-
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-for epoch in range(2):  # loop over the dataset multiple times
-
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs
-        inputs, labels = data
-
-        optimizer.zero_grad()
-
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        running_loss += loss.item()
-        if i % 2000 == 1999:
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
-print('Finished Training!')
-
-correct = 0
-total = 0
-with torch.no_grad():
-    for data in testloader:
-        images, lables = data
-        outputs = net(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += lables.size(0)
-        correct += (predicted == lables).sum().item()
-print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
---
+++ readme.md
@@ -0,0 +1,5 @@
+# 图像分类器
+## 1、加载并归一化数据
+torchvision数据集的输出是在范围[0, 1]之间的PILImage，我们将它归一化范围为[-1, 1]之间的张量Tensors
+torchvision-transform的一些方法介绍和使用
+https://pytorch-cn.readthedocs.io/zh/latest/torchvision/torchvision-transform/
